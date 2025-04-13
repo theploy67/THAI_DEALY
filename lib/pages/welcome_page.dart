a8e3a17e62ceb971503e89e08c24cc8cc7782c29
nsfxuxe13 <nutt.sai@student.mahidol.edu>
2025-04-13 23:07:13 +0700

    welcome_page.dart

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,1 +1,44 @@
 // By Fluke page7 welcome_page
+import 'package:flutter/material.dart';
+
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFF4C430), // เหลือง
+              Colors.white,      // ไล่ลงขาว
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // โลโก้
+              SizedBox(
+                width: 250,
+                height: 250,
+                child: Image.asset(
+                  'assets/images/THAI DR.png', 
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
